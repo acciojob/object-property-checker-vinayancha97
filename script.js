c90810eb46fa9f726a92a202979a00ebc87fcc94
vinayancha97 <135926111+vinayancha97@users.noreply.github.com>
2023-07-30 09:29:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	return key in sampleObject;
 }
+const keyToCheck = "age";
+const hasAgeKey = hasKey(keyToCheck);
+console.log(hasAgeKey);
 
 // Do not change the code below
 const key = prompt("Enter Key.");
